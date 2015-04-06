a1a75a0e8f96c14ccc23b376e82d60712a3ef179
chan <chen2aaron@gmail.com>
2015-04-06 15:42:47 +0800

    add archives,aboutme,readmore,pages

--- article/views.py
+++ article/views.py
@@ -5,10 +5,18 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 from django.http import Http404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
-def home (request):
-    post_list = Article.objects.all()  #获取全部的Article对象
+def home(request):
+    posts = Article.objects.all()  #获取全部的Article对象
+    paginator = Paginator(posts, 5) #每页显示两个
+    page = request.GET.get('page')
+    try :
+        post_list = paginator.page(page)
+    except PageNotAnInteger :
+        post_list = paginator.page(1)
+    except EmptyPage :
+        post_list = paginator.paginator(paginator.num_pages)
     return render(request, 'home.html', {'post_list' : post_list})
 
 def detail(request, id):
@@ -17,6 +25,24 @@ def detail(request, id):
 	except Article.DoesNotExist:
 		raise Http404
 	return render(request, 'post.html', {'post': post})
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category_iexact = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
 
 # def detail(request, my_args):
 # 	post = Article.objects.all()[int(my_args)]
--- ccblog/urls.py
+++ ccblog/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'article.views.home'),
     url(r'^(?P<id>\d+)/$', 'article.views.detail', name = 'detail'),
+    url(r'^archives/$', 'article.views.archives', name = 'archives'),
+    url(r'^aboutme/$', 'article.views.about_me', name = 'about_me'),
+    url(r'^tag(?P<tag>\w+)/$', 'article.views.search_tag', name = 'search_tag'),
+
+
     # url(r'^temp/$', 'article.views.temp'),
     # url(r'^bootstrap/$', 'article.views.bootstrap'),
 ]
